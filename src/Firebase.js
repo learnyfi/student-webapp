3d1469d3111e880e73ddb40e3536888a45c2e2f0
Naman <namanbajpai23@gmail.com>
2024-08-28 23:42:53 +0530

    Checking for workflow again removing error

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,7 +2,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const auth = getAuth(app);
 
 // Export the auth object so it can be used in other files
